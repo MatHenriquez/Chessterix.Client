5f3d8d8176b26e459b7fa6c23e1b472cc6873556
MatHenriquez <matias.henriquez.dev@gmail.com>
2025-06-29 17:30:35 -0300

    refactor: rename SignInButton to AuthButton and update component definition

--- app/components/AuthButton.tsx
+++ app/components/AuthButton.tsx
@@ -1,7 +1,11 @@
 import { renameToKebabCase } from '@/utils/naming-conventions-converter';
-import React from 'react';
+import React, { FC } from 'react';
 
-const SignInButton = ({ showAuthModal, setShowAuthModal, text }: props) => (
+const AuthButton: FC<props> = ({
+  showAuthModal,
+  setShowAuthModal,
+  text
+}: props) => (
   <button
     className="block w-full text-xl rounded lg:text-lg bg-red-main px-12 py-3 font-medium text-white shadow-md hover:bg-kimono-200 focus:outline-none ring-1 ring-white focus:ring-4 active:bg-kimono-700 sm:w-auto shadow-red-50/75"
     data-cy={renameToKebabCase(text + '-button')}
@@ -17,4 +21,4 @@ type props = {
   text: string;
 };
 
-export default SignInButton;
+export default AuthButton;
