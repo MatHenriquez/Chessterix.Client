aa3d3d81206dcd3d825eed2954579cfa9b91de92
Matías Henríquez <matias.henriquez.dev@gmail.com>
2025-07-03 21:05:23 -0300

    feat: define game status constants and initialize game state structure

--- app/constants/init-game-state.ts
+++ app/constants/init-game-state.ts
@@ -1,8 +1,17 @@
 import { createInitialPosition } from '@/board/helpers/position';
 import { turns } from './turns';
 
+export const STATUS = {
+  ONGOING: 'onGoing',
+  PROMOTING: 'promoting',
+  WHITE_WINS: 'whiteWins',
+  BLACK_WINS: 'blackWins'
+};
+
 export const initGameState = {
   position: [createInitialPosition()],
   turn: turns.WHITE,
-  candidateMoves: [] as number[][]
+  candidateMoves: [] as number[][],
+  status: STATUS.ONGOING,
+  promotionSquare: null
 };
