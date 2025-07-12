66af8edd19c7b12633815ee3d607ff06598ac32d
Matías Henríquez <matias.henriquez.dev@gmail.com>
2025-07-12 17:20:29 -0300

    fix: remove redundant import statement for initGameState

--- app/core/reducer/actions/game.ts
+++ app/core/reducer/actions/game.ts
@@ -1,3 +1,4 @@
+import { initGameState } from '@/core/constants/init-game-state';
 import actionTypes from '../actionTypes';
 
 export const updateCastling = (direction: { w: string; b: string }) => {
@@ -6,5 +7,47 @@ export const updateCastling = (direction: { w: string; b: string }) => {
     payload: {
       castleDirection: direction
     }
+  };
+};
+
+export const detectStalemate = () => {
+  return {
+    type: actionTypes.STALEMATE,
+    payload: null
+  };
+};
+
+export const detectInsufficientMaterial = () => {
+  return {
+    type: actionTypes.INSUFFICIENT_MATERIAL,
+    payload: null
+  };
+};
+
+export const detectThreefoldRepetition = () => {
+  return {
+    type: actionTypes.THREEFOLD_REPETITION,
+    payload: null
+  };
+};
+
+export const detectFiftyMoveRule = () => {
+  return {
+    type: actionTypes.FIFTY_MOVE_RULE,
+    payload: null
+  };
+};
+
+export const detectCheckmate = (winner: string) => {
+  return {
+    type: actionTypes.WIN,
+    payload: { winner }
+  };
+};
+
+export const setupNewGame = () => {
+  return {
+    type: actionTypes.NEW_GAME,
+    payload: initGameState
   };
 };
