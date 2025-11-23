31a4295176508ff1271d23d52652505ad3ff8ec0
MatHenriquez <matias.henriquez.dev@gmail.com>
2025-11-22 21:51:36 -0300

    feat: add EmailValidation component for handling email validation process

---
+++ app/EmailValidation/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useSearchParams, useRouter } from 'next/navigation';
+import React from 'react';
+import ValidationContainer from './components/ValidationContainer';
+import ValidationCard from './components/ValidationCard';
+import ValidationTitle from './components/ValidationTitle';
+import LoadingState from './components/LoadingState';
+import SuccessState from './components/SuccessState';
+import ErrorState from './components/ErrorState';
+import { useEmailValidation } from './hooks/useEmailValidation';
+
+const EmailValidation: React.FC = () => {
+    const EMAIL_VALIDATION_TOKEN = "token";
+    const searchParams = useSearchParams();
+    const validationToken = searchParams.get(EMAIL_VALIDATION_TOKEN);
+    const router = useRouter();
+    
+    const { validationStatus, errorMessage } = useEmailValidation(validationToken);
+
+    const handleGoHome = () => {
+        router.push('/');
+    };
+
+    return (
+        <ValidationContainer>
+            <ValidationCard>
+                <ValidationTitle />
+                {validationStatus === 'loading' && <LoadingState />}
+                {validationStatus === 'success' && <SuccessState onGoHome={handleGoHome} />}
+                {validationStatus === 'error' && <ErrorState errorMessage={errorMessage} onGoHome={handleGoHome} />}
+            </ValidationCard>
+        </ValidationContainer>
+    );
+};
+
+export default EmailValidation;
